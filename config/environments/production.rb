21f9f25206b1631b9443c7adffff1301f1e029b8
Paul Ashour <paul.ashour@gmail.com>
2019-05-12 19:11:05 -0600

    begin devise setup
    add devise gem
    add user devise model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  # Configure action_mailer for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
