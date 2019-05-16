d5798b13bad6cf1b7675ec1975906e54cdb8e1ac
Paul Ashour <paul.ashour@gmail.com>
2019-05-16 08:50:20 -0600

    add nav styling and fix login demo
    add (incomplete) styling in css
    remove "has secure password" from user since devise handles that
    add show page handler to user controller to demo logged_in state

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :singer
-  has_secure_password
+  # has_secure_password
   
 end
