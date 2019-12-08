85e03ac0842591518244a93e42028d224497ad96
Paul Ashour <paul.ashour@gmail.com>
2019-12-07 18:04:57 -0700

    Precompile in preparation for Heroku
    The /bin/precompile that I've created has a quick workaround for the
    secret key that throws an error even when config.require_master_key
    is set to false.
    Precompiling also necessitated a "harmony:true" switch for uglifier
    in the production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony:true) # changed from bare ":uglifier" to allow for ES6 syntax as per instructions on the uglifier Github page
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
